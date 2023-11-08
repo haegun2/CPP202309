884dbff14a28ccf5e1325e7f7919c5d4f5c2d9cc
haegun2 <143983747+haegun2@users.noreply.github.com>
2023-11-08 15:02:34 +0900

    #3 fix

---
+++ challenge/week2/temperature2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double f_temp;
+	double c_temp;
+
+	cout << "¼·¾¾¿Âµµ: ";
+	cin >> c_temp;
+
+	f_temp = c_temp * 9 / 5 + 32;
+
+
+	cout << "È­¾¾¿Âµµ= " << f_temp << endl;
+	return 0;
+
+}
